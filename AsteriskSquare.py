c8bb3e5ab3d26826eaf49ecda2eaaa249e5e6876
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-02-13 22:51:37 -0500

    Add files via upload

---
+++ AsteriskSquare.py
@@ -0,0 +1,25 @@
+import time
+
+class rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.topAndBottom = ""
+        self.mid = ""
+
+    def draw(self):
+        self.topAndBottom += "* "
+        for i in range(self.width - 2):
+            self.topAndBottom += "* "
+        self.topAndBottom += "*"
+        print(self.topAndBottom)
+        self.mid += "*"
+        for i in range(len(self.topAndBottom)-2):
+            self.mid += " "
+        self.mid += "*"
+        for i in range(self.height-2):
+            print(self.mid)
+        print(self.topAndBottom)
+
+newSquare = rectangle(5, 5)
+newSquare.draw()
