29448c74537192ffbe20fbfdde72fa76562733a9
GrahamJoonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2021-02-13 23:08:10 -0500

    Fixing side length of 1 or less inaccuracies

--- AsteriskSquare.py
+++ AsteriskSquare.py
@@ -6,18 +6,30 @@ class rectangle:
         self.mid = ""
 
     def draw(self):
-        self.topAndBottom += "* "
-        for i in range(self.width - 2):
+        if self.width > 1 and self.height > 1:
             self.topAndBottom += "* "
-        self.topAndBottom += "*"
-        print(self.topAndBottom)
-        self.mid += "*"
-        for i in range(len(self.topAndBottom)-2):
-            self.mid += " "
-        self.mid += "*"
-        for i in range(self.height-2):
-            print(self.mid)
-        print(self.topAndBottom)
+            for i in range(self.width - 2):
+                self.topAndBottom += "* "
+            self.topAndBottom += "*"
+            print(self.topAndBottom)
+            self.mid += "*"
+            for i in range(len(self.topAndBottom)-2):
+                self.mid += " "
+            self.mid += "*"
+            for i in range(self.height-2):
+                print(self.mid)
+            print(self.topAndBottom)
+        elif self.width == 1 and self.height > 0:
+            for i in range(self.height):
+                print("*")
+        elif self.height == 1 and self.width > 0:
+            self.topAndBottom += "* "
+            for i in range(self.width - 2):
+                self.topAndBottom += "* "
+            self.topAndBottom += "*"
+            print(self.topAndBottom)
+        else:
+            print("Cannot show a rectangle with a side length of Zero or less.")
 
 newRectangle = rectangle(5, 5)
 newRectangle.draw()
